e126806ebc5096f52aa6456392d846a02f5eaff9
conGaStuer <xuankhuong1402@.com>
2023-10-23 14:08:49 +0700

    my change

--- Source.cpp
+++ Source.cpp
@@ -3,10 +3,14 @@
 using namespace std;
 int tong(int a, int b);
 int hieu(int a, int b);
-int tich(int a, int b);
-float thuong(int a, int b);
+int tich(int a, int b) {
+	return a * b;
+}
+float thuong(int a, int b) {
+	return a / b ;
+}
 int main()
-{
+{	
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
 	system("pause");
 	return 0;
